1785c4fa4944724f171ac1923bcd28758a3770ce
Kartikaggarwal98 <Kartikaggarwal98@gmail.com>
2020-04-22 22:11:04 +0530

    config vars to heroku

--- prediction.py
+++ prediction.py
@@ -3,18 +3,28 @@ from praw.models import MoreComments
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle, json
+from os import environ
 
-with open('config_file.json') as config_file:
-    config = json.load(config_file)
+config={}
+
+config['c_id'] = environ.get("c_id")
+config['c_secret']=environ.get("c_secret")
+config['u_a']=environ.get("u_a")
+config['usrnm']=environ.get("usrnm")
+config['passwd']=environ.get("passwd")
+
+# with open('config_file.json') as config_file:
+    # config = json.load(config_file)
 
 
 def predict(url):
 	# print (config)
 	reddit = praw.Reddit(client_id = config['c_id'],
-	 client_secret = config['c_secret'],
-	 user_agent = config['u_a'],
-	 username = config['usrnm'],
-	 password = config['passwd'])
+	client_secret = config['c_secret'],
+	user_agent = config['u_a'],
+	username = config['usrnm'],
+	password = config['passwd']
+	)
 
 	idx_flair= ['AskIndia', 'Business/Finance', 'Food', 'Non-Political', 'Photography', 'Policy/Economy', 'Politics', 'Science/Technology','Sports']
 
